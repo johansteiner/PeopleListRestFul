1800e18741ce9646f410205ad4a938e6ac2ff90a
johansteiner <johan.steiner@he-arc.ch>
2016-10-20 09:23:41 +0200

    people bean

---
+++ src/java/ch/hearc/ig/odi/peoplelist/bean/PeopleListBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peoplelist.bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author johan.steiner
+ */
+@Named(value = "peopleListBean")
+@RequestScoped
+public class PeopleListBean {
+
+    /**
+     * Creates a new instance of PeopleListBean
+     */
+    public PeopleListBean() {
+    }
+    
+}
